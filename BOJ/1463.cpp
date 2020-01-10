509016e407b0bf0154e0f0aa6aaee9c22da5a2b3
sohyeon <sohyun1018@gmail.com>
2020-01-10 23:02:18 +0900

    [update] BOJ_1463_1로 만들기

---
+++ BOJ/1463.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+ 
+using namespace std;
+typedef long long ll;
+ 
+ll dp[1000001];
+const ll inf = 2000000;
+ 
+int main()
+{
+    ll n;
+    fill(dp, dp + 1000000, inf);
+    cin >> n;
+    dp[n] = 0;
+    for (int i = n; i > 0; i--)
+    {
+        if (dp[i] == inf)continue;
+        if (i % 3 == 0)dp[i / 3] = min(dp[i / 3], dp[i] + 1);
+        if (i % 2 == 0)dp[i / 2] = min(dp[i / 2], dp[i] + 1);
+        if (i >= 2)dp[i - 1] = min(dp[i - 1], dp[i] + 1);
+    }
+    cout << dp[1];
+    return 0;
+}
