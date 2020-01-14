e553e97517ca3245015ad4d28a1071bdf81bcbe2
sohyeon <sohyun1018@gmail.com>
2020-01-14 16:41:25 +0900

    [solve] BOJ_2667_단지번호붙이기 (DFS, BFS)

--- BOJ/2667.cpp
+++ BOJ/2667.cpp
@@ -1,24 +1,43 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 const int dx[] = {-1,0,0,1}, dy[] = {0,-1,1,0};
 vector<int> res;
-int n;
+int n, cnt;
 int map[26][26];
-bool check[26][26];
-int dfs(int x, int y) {
-    int temp = 0;
+void dfs(int x, int y) {
+    map[x][y] = 0;
+    cnt++;
     for(int i=0;i<4;i++){
         int nx = x+ dx[i], ny = y + dy[i];
         if (nx <0 || nx >= n || ny <0 || ny >= n) continue;
-        if(map[nx][ny]==1 && !check[nx][ny]) {
-            check[nx][ny] = true;
-            temp++;
-            temp += dfs(nx,ny);
+        if(map[nx][ny]==1) {
+            map[nx][ny] = 0;
+            dfs(nx,ny);
         }
     }
-    return temp;
+}
+void bfs(int i, int j){
+    queue<pair<int, int> > q;
+    q.push(make_pair(i,j));
+    map[i][j] = 0;
+    cnt++;
+    while(!q.empty()){
+        int x = q.front().first, y = q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i], ny = y + dy[i];
+            if(nx <0 || nx >=n || ny <0 || ny >= n) continue;
+            if(map[nx][ny] == 1) {
+                map[nx][ny] = 0;
+                cnt++;
+                q.push(make_pair(nx,ny));
+            }
+            
+        }
+    }
 }
 int main(){
     scanf("%d", &n);
@@ -29,9 +48,11 @@ int main(){
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(map[i][j] == 1 && !check[i][j]){
-                check[i][j] = true;
-                res.push_back(dfs(i,j)+1);
+            if(map[i][j] == 1){
+                cnt = 0;
+                // dfs(i,j);
+                bfs(i,j);
+                res.push_back(cnt);
             }
         }
     }
@@ -40,6 +61,5 @@ int main(){
     for(int i=0;i<res.size();i++){
         printf("%d\n",res[i]);
     }
-
     return 0;
 }
