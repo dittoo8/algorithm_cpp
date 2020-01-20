15693e5321ee54d18d23a33e4f9267bb6b7b03ab
sohyeon <sohyun1018@gmail.com>
2020-01-20 21:30:51 +0900

    [solve] BOJ_11053_가장 긴 증가하는 부분 수열

---
+++ BOJ/11053.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+using namespace std;
+int n;
+int a[1002];
+int d[1002];
+int main(){
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++){
+        scanf("%d", &a[i]);
+    }
+    d[1]= 1;
+    int max = 0;
+    for(int i=1;i<=n;i++){
+        int tmp =0;
+        for(int j=1;j<i;j++){
+            if(a[j]<a[i]) {
+                if(d[j] > tmp) tmp = d[j];
+            } 
+        }
+        d[i] = tmp +1;
+        if (d[i] > max) max =d[i];
+        
+    }
+    printf("%d", max);
+    return 0;
+}
