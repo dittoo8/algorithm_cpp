5182b9758f0cf5cca3e10539a361302a74e0cb77
sohyeon <sohyun1018@gmail.com>
2020-01-31 17:32:15 +0900

    [solve] BOJ_11376_열혈강호 2

---
+++ BOJ/11376.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#define MAX 1002
+using namespace std;
+int n,m;
+int w[MAX];
+vector<int> p[MAX];
+bool check[MAX];
+bool dfs(int x){
+    for(int i=0;i<p[x].size();i++){
+        int y = p[x][i];
+        if (check[y]) continue;
+        check[y] = true;
+        if(w[y]==0 || dfs(w[y])) {
+            w[y] = x;
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i=1;i<=n;i++){
+        int a;
+        cin >> a;
+        while(a--){
+            int b;
+            cin >> b;
+            p[i].push_back(b);
+        }
+    }
+    int cnt = 0;
+    
+    for(int j=0;j<2;j++){
+        for(int i=1;i<=n;i++){
+        fill(check,check+MAX, false);
+        if (dfs(i)) cnt++;
+      }
+    }
+    cout << cnt << endl;
+
+    return 0;
+}
