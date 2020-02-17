645947ce0e7d1c39fca8fe3e73ed4fee4d77eeb6
sohyeon <sohyun1018@gmail.com>
2020-02-17 15:35:41 +0900

    [solve] programmers_Lv2_프렌즈4블록 (개선)

--- programmers_level_2/firends_4_block.cpp
+++ programmers_level_2/firends_4_block.cpp
@@ -6,24 +6,20 @@ int solution(int m, int n, vector<string> board) {
     int answer = 0;
     bool flag = false;
     while (flag == false) {
-        vector<vector<bool> > visit(m, vector<bool>(n));
+        bool visit[31][31] = {false, };
         flag = true;
         for (int i = 0; i <m-1;i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (board[i][j] == 0)
-                    continue;
-                if ((board[i][j] == board[i][j + 1])&& (board[i][j]== board[i + 1][j])&&(board[i][j]== board[i + 1][j + 1])) {
-                    visit[i][j] = 1;
-                    visit[i][j+1] = 1;
-                    visit[i+1][j] = 1;
-                    visit[i + 1][j + 1] = 1;
+            for (int j = 0; j < n-1; j++) {
+                if (!board[i][j]) continue;
+                if ((board[i][j] == board[i][j + 1]) && (board[i][j] == board[i + 1][j]) &&(board[i][j] == board[i + 1][j + 1])) {
+                    visit[i][j] = visit[i][j+1] = visit[i+1][j] = visit[i + 1][j + 1] = true;
                     flag = false;
                 }
             }
         }
         for (int i = 0; i < m; i++) {   //제거
             for (int j = 0; j < n; j++) {
-                if (visit[i][j]==1) {
+                if (visit[i][j]) {
                     answer++;
                     for (int k = i-1; k >=0; k--) {
                         board[k + 1][j] = board[k][j];
