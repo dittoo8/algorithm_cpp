ae1d4068bb0ee0c38c9a48356beab3d35ed963eb
sohyeon <sohyun1018@gmail.com>
2020-02-20 23:44:26 +0900

    [solve] programmer_Lv2_괄호 변환 (2020 KAKAO BLIND RECRUITMENT) (개선)

--- programmers_level_2/bracket_convert.cpp
+++ programmers_level_2/bracket_convert.cpp
@@ -1,66 +1,64 @@
 #include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <stack>
-
 using namespace std;
-bool isRight(string p){
-    stack<char> st;
-    for(int i = 0; i < p.size(); i++)
-    {
-        if(p[i] == '(') {
-            st.push(p[i]);
+string eraseU(string u){
+    string newU = "";
+    int lastIdx = u.length()-1;
+    for(int i=1;i<lastIdx;i++){
+        if(u[i]=='(') {
+            newU+=')';
         } else {
-            if(st.empty()){
-                return 0;
-            }
-            st.pop();
+            newU+='(';
         }
     }
-    if(st.empty()){
-        return true;
-    } else{
-        return 0;
+    return newU;
+}
+bool correct(string u){
+    stack<int> st;
+    if (u[0]==')') return false;
+    for(int i=0;i<u.length();i++){
+        if (u[i]=='('){
+            st.push(1);
+        } else {
+            if (st.empty()) return false;
+            else st.pop();
+        }
     }
+    if (st.empty()) return true;
+    return false;
+}
+bool balance (string p, int s, int e){
+    int a=0,b=0;
+    for(int i=s;i<=e;i++){
+        if(p[i]=='(') a++;
+        if(p[i]==')') b++;
+    }
+    if (a==b) return true;
+    else return false;
 }
 string solution(string p) {
-    string answer = "";
-    if (p == "") return answer;
-    int op = 0;
-    int cl = 0;
-    string u,v;
-    for(int i=0;i<p.length();i++){
-        if (p[i]=='('){
-            op++;
-        } else {
-            cl++;
-        }
-        if(op == cl) {
-            u = p.substr(0, op+cl);
-            v = p.substr(op+cl, p.size());    
+    if(p=="") return "";
+    string u = "";
+    string v = "";
+    for(int i=1;i<p.length();i+=2){
+        if (balance(p,0,i) && balance(p,i+1,p.length()-1)){
+            u = p.substr(0,i+1);
+            v = p.substr(i+1,p.length()-i+1);
             break;
         }
     }
-    if(isRight(u)){
-        answer += u;
-        string temp = solution(v);
-        answer += temp;
+    if (correct(u)){
+        return u+=solution(v);
     } else {
         string temp = "(";
-        string tempV = solution(v);
-        temp +=tempV;
-        temp += ")";
-        string temp3 = u.substr(1, u.size()-2);
-        for(int i=0;i<temp3.length();i++){
-            if (temp3[i] == '(') {
-                temp += ")";
-            } else {
-                temp += "(";
-            }
-        }
-        answer = temp;
+        temp+=solution(v);
+        temp+=')';
+        temp+=eraseU(u);
+        return temp;
     }
-    return answer;
 }
 int main(){
 
