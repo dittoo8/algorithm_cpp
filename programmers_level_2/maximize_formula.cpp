796819d5dd9489421f43765952968115916e8b22
sohyeon <sohyun1018@gmail.com>
2020-07-03 13:52:24 +0900

    [solve] programmers_Lv2_수식 최대화

---
+++ programmers_level_2/maximize_formula.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(string expression) {
+    long long answer = 0;
+    char op[3]={'+','-','*'};
+    sort(op,op+3);
+    long long now=0;
+    vector<long long> nums;
+    vector<long long> newnums;
+    vector<char> ops;
+    for(int i=0;i<expression.length();i++){
+        if(isdigit(expression[i])){
+            now*=10;
+            now+=expression[i]-'0';
+        }else {
+            nums.push_back(now);
+            now = 0;
+            ops.push_back(expression[i]);
+        }
+    }
+    nums.push_back(now);
+    int nsize = (int)nums.size();
+    vector<bool> check;
+    check.resize(nsize,false);
+    newnums.resize(nsize);
+    do{
+        fill(check.begin(),check.end(),0);
+        newnums.assign(nums.begin(),nums.end());
+        for(int i=0;i<3;i++){ // 연산자 우선순위
+            for(int j=0;j<ops.size();j++){
+                if (ops[j]==op[i]){
+                    int k,t;
+                    for(k=j;k>=0;k--){
+                        if (!check[k]) break;
+                    }
+                    for(t=j+1;t<nums.size();t++){
+                        if (!check[t]) break;
+                    }
+                    check[t]=true;
+                    switch(op[i]){
+                        case '*' : newnums[k]*=newnums[t];
+                            break;
+                        case '+' : newnums[k]+=newnums[t];
+                            break;
+                        case '-' : newnums[k]-=newnums[t];
+                            break;
+                    }
+
+                }
+            }
+        }
+        newnums[0]= abs(newnums[0]);
+        if (newnums[0] > answer) answer = newnums[0];
+    } while(next_permutation(op,op+3));
+    return answer;
+}
+int main(){
+    cout << solution("100-200*300-500+20") << endl;
+    return 0;
+}
