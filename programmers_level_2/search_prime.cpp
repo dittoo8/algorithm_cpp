924567bacff907b141b8799d68c8e8e10fd6e140
sohyeon <sohyun1018@gmail.com>
2020-07-09 17:48:55 +0900

    [solve] programmers_Lv2_소수 찾기_re

--- programmers_level_2/search_prime.cpp
+++ programmers_level_2/search_prime.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool checkNum(int i, string numbers){
-    vector<bool> visited(numbers.size()+1);
-    while(i){
-        bool flag = false;
-        int temp = i%10;
-        for(int j=0;j<=numbers.size();j++){
-            if(temp == numbers[j]-'0' && visited[j]==false) {
-                flag = true;
-                visited[j] = true;
-                break;
-            }
-        }
-        if(!flag) return false;
-        i /=10;
+int cnt[10];
+bool checkNum(int i){
+    string s = to_string(i);
+    int tmpcnt[10];
+    memcpy(tmpcnt,cnt,sizeof(tmpcnt));
+    for(int i=0;i<s.length();i++){
+        tmpcnt[s[i]-'0']--;
+        if(tmpcnt[s[i]-'0'] < 0) return false;
     }
     return true;
 }
 int solution(string numbers) {
     int answer = 0;
     sort(numbers.begin(),numbers.end(),greater<int>());
+    for(int i=0;i<numbers.length();i++){
+        cnt[numbers[i]-'0']++;
+    }
     vector<bool> temp(stoi(numbers)+1, false);
     for(int i=2;i<=stoi(numbers); i++){
-        if (!temp[i] && checkNum(i,numbers)) answer++;
+        if (!temp[i] && checkNum(i)) answer++;
         if(!temp[i]){
             for(int j=i;j<=stoi(numbers);j+=i) temp[j] = true;
         }
