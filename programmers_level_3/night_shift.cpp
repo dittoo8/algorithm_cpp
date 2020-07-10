9e1e52181405050f849eb68ddc288c84bdc1d26c
sohyeon <sohyun1018@gmail.com>
2020-07-11 00:53:18 +0900

    [solve] programmers_Lv3_야근 지수

---
+++ programmers_level_3/night_shift.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+    priority_queue<int> q(works.begin(), works.end());
+    for (int i = 0; i < n; i++) {
+        if (q.top() > 0) {
+            int tmp = q.top();
+            q.pop();
+            q.push(tmp-1);
+        }
+    }
+    long long answer = 0;
+    while(!q.empty()) {
+        answer += (long long)q.top() * (long long)q.top();
+        q.pop();
+    }
+    return answer;
+}
+int main(){
+    vector<int> v = {4,3,3};
+    cout <<  solution(4,v) <<endl;
+    return 0;
+}
