73799583ff9d15b837f9b8f4383dfd2b15dc7a6f
sohyeon <sohyun1018@gmail.com>
2020-07-24 19:05:26 +0900

    [solve] programmers_Lv3_보석 쇼핑_re

--- programmers_level_3/Jewelry_shopping.cpp
+++ programmers_level_3/Jewelry_shopping.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
 #include <map>
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
     vector<int> answer;
-    set<string> st;
     map<string, int> mp;
     for(int i=0;i<gems.size();i++){
-        st.insert(gems[i]);
         mp[gems[i]]=0;
     }
-    int jSize = st.size();
-    int r = 0, l =0;
+    int jSize = mp.size();
+    int r = 0, l = 0;
     int last = (int)gems.size();
     int nowcnt = 0;
     answer.push_back(0);
     answer.push_back(last);
-    while(l<last){
-        if (nowcnt == jSize) {
+    while(true){
+        if (nowcnt >= jSize){
+            if(mp[gems[l]]-1==0){
+                nowcnt--;
+            }
+            mp[gems[l++]]--;
+        } else if (r == last) break;
+        else {
+            if(mp[gems[r]]==0) nowcnt++;
+            mp[gems[r++]]++;
+        }
+        if(nowcnt == jSize) {
             if (r-l < answer[1]-answer[0]){
                 answer[0]=l;
                 answer[1]=r;
             }
         }
-        if (nowcnt < jSize && r+1 < last){
-            if(mp[gems[r]]==0) {
-               nowcnt++;
-            }
-            mp[gems[r]]++;
-            r++;
-        }else if(l+1 < last){
-            if(mp[gems[l]]-1 ==0){
-                nowcnt--;
-            }
-             mp[gems[l]]--;
-            l++;
-        }else if(l+1 == last) break;
     }
     answer[0]++;
     return answer;
