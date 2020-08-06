6200d7b371fc53a6065900e2646ee029af8b787b
sohyeon <sohyun1018@gmail.com>
2020-08-06 22:52:07 +0900

    [solve] BOJ_2292_벌집

---
+++ BOJ/2292.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin >> n;
+    int cnt = 1;
+    long long range =  1, tmp = 1;
+    while (true){
+        if (range >= n){
+            break;
+        }
+        tmp = 6 * (cnt++);
+        range+=tmp;
+    }
+    cout << cnt;
+    return 0;
+}
