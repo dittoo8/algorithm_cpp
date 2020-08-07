a2fd1a8f19c7f19ae63139f00dbe74280ec39c48
sohyeon <sohyun1018@gmail.com>
2020-08-07 21:05:29 +0900

    [solve] BOJ_1193_분수찾기

---
+++ BOJ/5427.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string maps[1001];
+int main(){
+    int tc,w,h;
+    cin >> tc;
+    while(tc--){
+        cin >> w >> h;
+        for(int i=0;i<h;i++){
+            cin >> maps[i];
+            for(int j=0;j<w;j++){
+                if (maps[i][j])
+            }
+        }
+    }
+    return 0;
+}
