0c2e1b430982174a56a35dee66623edae93e4c03
sohyeon <sohyun1018@gmail.com>
2020-08-22 17:17:22 +0900

    [solve] BOJ_11729_하노이 탑 이동 순서

--- BOJ/11729.cpp
+++ BOJ/11729.cpp
@@ -4,20 +4,17 @@ using namespace std;
 vector<pair<int, int> > v;
 void hanoi(int x,int s, int m, int e){
     if (x==0) return;
-    cout << "first " << endl;
     hanoi(x-1,s,e,m);
-    cout << "x : " << x << ", s: " << s <<", e : "<< e << '\n';
     v.push_back(make_pair(s,e));
-    cout <<  "second  " << endl;
     hanoi(x-1,m,s,e);
 }
 int main(){
     int n;
     cin >> n;
     hanoi(n,1,2,3);
-    // cout << (int)v.size() << '\n';
-    // for(int i=0;i<v.size();i++){
-    //     cout << v[i].first <<' ' << v[i].second << '\n';
-    // }
+    cout << (int)v.size() << '\n';
+    for(int i=0;i<v.size();i++){
+        cout << v[i].first <<' ' << v[i].second << '\n';
+    }
     return 0;
 }
