87e649d3792651972317ee78ba954f72e5e122e8
sohyeon <sohyun1018@gmail.com>
2020-09-07 16:18:35 +0900

    [solve] BOJ_11279_최대 힙

--- BOJ/11279.cpp
+++ BOJ/11279.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int heap[100001];
 int lastIdx;
@@ -8,41 +9,38 @@ void pop() {
         return;
     }
     cout << heap[1] << '\n';
-    swap(heap[1],heap[lastIdx]);
-    lastIdx--;
-    int now = 1;
-    int child;
-    do {
-        child = 2*now;
-        if (child+1 <= lastIdx && heap[child] < heap[child+1]) child++;
-        if (child <= lastIdx && heap[now] < heap[child]){
-            swap(heap[now],heap[child]);
+    heap[1] = heap[lastIdx];
+    heap[lastIdx--] = 0;
+    for (int i=1; i*2 <= lastIdx;) {
+        if (heap[i] > heap[i*2] && heap[i] > heap[i*2+1]) {
+            break;
+        } else if (heap[i*2] > heap[i*2+1]) {
+            swap(heap[i], heap[i*2]);
+            i = i*2;
+        } else {
+            swap(heap[i], heap[i*2+1]);
+            i = i*2+1;
         }
-        now = child;
-    } while( child <= lastIdx);
+    }
 }
 void push(int x) {
-    lastIdx++;
-    heap[lastIdx] = x;
-    int now = lastIdx;
-    int parent = now/2;
-    while (parent>=1){
-        if (heap[parent] < heap[now]){
-            swap(heap[parent],heap[now]);
+    heap[++lastIdx] = x;
+    for (int i=lastIdx; i>1; i/=2) {
+        if (heap[i/2] < heap[i]) {
+            swap(heap[i/2],heap[i]);
         } else break;
-        now = parent;
     }
 }
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+    cin.tie(0); cout.tie(0);
     int t,x;
     cin >> t;
     while (t--) {
+        int x;
         cin >> x;
-        if (x){
-            push(x);
-        } else pop();
+        if (x) push(x);
+        else pop();
     }
     return 0;
 }
