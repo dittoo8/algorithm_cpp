759603b7e1d38bedc03f9d7c1e129e4c54de2d9b
sohyeon <sohyun1018@gmail.com>
2020-10-05 01:22:32 +0900

    [solve] BOJ_15989_1,2,3 더하기 4_re

--- BOJ/15989.cpp
+++ BOJ/15989.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
-int dp[2][10001];
-int main()
-{
+int d[10001];
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    dp[0][2] = 1;
-    for (int i = 3; i <= 10000; i++) {
-        dp[0][i] = dp[0][i - 2] + 1;
-        dp[1][i] = dp[0][i - 3] + dp[1][i - 3] + 1;
-    }
     int T;
     cin >> T;
+    d[0]=1;
+    for(int i=1;i<=3;i++){
+        for(int j=i;j<=10000;j++){
+            d[j]+=d[j-i];
+        }
+    }
+    
     while (T--) {
         int n;
         cin >> n;
-        cout << 1+dp[0][n]+dp[1][n] << '\n';
+        cout << d[n] << '\n';
     }
     return 0;
 }
