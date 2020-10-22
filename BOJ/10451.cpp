d5a4eb55593c258f392898ae2afc7dd73637ce78
sohyeon <sohyun1018@gmail.com>
2020-10-22 16:55:03 +0900

    [solve] BOJ_10451_순열 사이클_re

--- BOJ/10451.cpp
+++ BOJ/10451.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
 #define MAX 1001
 using namespace std;
-int ans,visit[MAX], adj[MAX];
-bool finish[MAX];
-stack<int> st;
-int dfs(int x){
-    visit[x] = x;
-    st.push(x);
-    int next = adj[x];
-    if(!visit[next]){
-        visit[x] = min(visit[x],dfs(next));
-    }else if (!finish[next]){
-        visit[x] = min(visit[x], visit[next]);
-    }
-    if(visit[x]==x){
-        ans++;
-        while(true){
-            int t = st.top();
-            st.pop();
-            finish[t] = true;
-            if (t == x) break;
-        }
-    }
-    return visit[x];
+int adj[MAX];
+bool visit[MAX];
+void dfs(int x){
+    if(visit[x]) return; // 이미 방문한 정점을 또 방문하면 cycle
+    visit[x] = true;
+    dfs(adj[x]);
 }
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int tc,n;
+    int tc,n,ans;
     cin >> tc;
     while(tc--){
         cin >> n;
         ans = 0;
-        while(!st.empty()) st.pop();
-        fill(&finish[0], &finish[n]+1,0);
         fill(&visit[0],&visit[n]+1,0);
-        int num;
         for(int i=1;i<=n;i++){
             cin >> adj[i];
         }
         for(int i=1;i<=n;i++){
-            if(!finish[i]){
+            if(!visit[i]){
                 dfs(i);
+                ans++;
             }
         }
         cout << ans << '\n';
