158f71001e1a183d4f7db8f6ebc73bdf9357e689
sohyeon <sohyun1018@gmail.com>
2020-11-08 17:31:01 +0900

    [solve] BOJ_2504_괄호의 값_re

--- BOJ/2504.cpp
+++ BOJ/2504.cpp
@@ -5,62 +5,43 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    stack<int> sSt,bSt;
-    stack<pair<int, int> > vSt;
+    stack<char> st;
     string str;
+    bool ck = false;
     int ans = 0;
+    int tmp = 1;
     cin >> str;
     for(int i=0;i<(int)str.length();i++){
-        if(str[i]=='('){
-            sSt.push(i);
-        }else if (str[i]=='['){
-            bSt.push(i);
+        if(str[i] == '('){
+            tmp*=2;
+            st.push('(');
+        }else if (str[i] == '['){
+            tmp*=3;
+            st.push('[');
         }else if (str[i] == ')'){
-            if(sSt.empty()){
-                cout << 0 << '\n';
-                return 0;
+            if(st.empty() || st.top()!='('){
+                ck = true;
+                break;
             }
-            if(sSt.top() == i-1){
-                vSt.push(make_pair(i,2));
-                sSt.pop();
-            }else {
-                int tmp = 0;
-                while(!vSt.empty() && sSt.top() <=vSt.top().first ){
-                    tmp+=vSt.top().second;
-                    vSt.pop();
-                }
-                tmp*=2;
-                vSt.push(make_pair(sSt.top(),tmp));
-                sSt.pop();
+            if(str[i-1] == '('){
+                ans+=tmp;
             }
-        }else if (str[i]==']'){
-            if(bSt.empty()){
-                cout << 0 << '\n';
-                return 0;
+            st.pop();
+            tmp/=2;
+        }else if (str[i] == ']'){
+            if(st.empty() || st.top()!='['){
+                ck = true;
+                break;
             }
-            if(bSt.top() == i-1){
-                vSt.push(make_pair(i,3));
-                bSt.pop();
-            }else {
-                int tmp = 0;
-                while(!vSt.empty() && bSt.top() <=vSt.top().first ){
-                    tmp+=vSt.top().second;
-                    vSt.pop();
-                }
-                tmp*=3;
-                vSt.push(make_pair(bSt.top(),tmp));
-                bSt.pop();
+            if(str[i-1] == '['){
+                ans+=tmp;
             }
-        }
-        if(sSt.empty() && bSt.empty()){
-            while(!vSt.empty()){
-                ans+=vSt.top().second;
-                vSt.pop();
-            }
+            st.pop();
+            tmp/=3;
         }
     }
-    if(sSt.empty() && bSt.empty()){
-        cout << ans << '\n';
-    }else cout << 0 << '\n';
+    if(ck || !st.empty()){
+        cout << 0 << '\n';
+    }else cout << ans << '\n';
     return 0;
 }
