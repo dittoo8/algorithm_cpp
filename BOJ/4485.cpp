44a5081acda9487293729f13817fb19148bf0433
sohyeon <sohyun1018@gmail.com>
2020-12-16 23:32:15 +0900

    [solve] BOJ_4485_녹색 옷 입은 애가 젤다지?_re

--- BOJ/4485.cpp
+++ BOJ/4485.cpp
@@ -4,6 +4,10 @@
 #define INF 987654321
 using namespace std;
 const int dx[]={0,0,-1,1},dy[]={-1,1,0,0};
+struct pos {
+    int c, x, y;
+    bool operator < (pos a)const { return c > a.c; }
+};
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -17,17 +21,19 @@ int main(){
             }
             fill(&costs[0][0], &costs[n][n]+1, INF);
             costs[0][0] = maps[0][0];
-            queue<pair<int, int> > q;
-            q.push(make_pair(0,0));
-            while(!q.empty()){
-                int x = q.front().first, y = q.front().second;
-                q.pop();
+            priority_queue<pos> pq;
+            pq.push({maps[0][0],0,0});
+            while(!pq.empty()){
+                int x = pq.top().x, y = pq.top().y;
+                int c = pq.top().c;
+                pq.pop();
+                if(c > costs[x][y]) continue;
                 for(int i=0;i<4;i++){
                     int nx = x +dx[i], ny = y+dy[i];
                     if(nx < 0 || nx >= n || ny <0 || ny >=n) continue;
-                    if(costs[nx][ny] > costs[x][y] + maps[nx][ny]){
+                    if(costs[nx][ny] > c + maps[nx][ny]){
                         costs[nx][ny] = costs[x][y] + maps[nx][ny];
-                        q.push(make_pair(nx,ny));
+                        pq.push({costs[nx][ny],nx,ny});
                     }
                 }
                 
