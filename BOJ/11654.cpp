9663f9bd4be42a0173fd698f5dc43394593a2961
sohyeon <sohyun1018@gmail.com>
2021-02-11 17:39:59 +0900

    [solve] BOJ_11654_아스키 코드

---
+++ BOJ/11654.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    char c;
+    int res;
+    cin >> c;
+    res = (int)c;
+    cout << res << '\n';
+    return 0;
+}
